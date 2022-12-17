0f360150559266ff4b890236c503d20dcef9b8e7
Adrian Steinle <atze92@gmail.com>
2022-12-17 09:07:42 +0000

    small azure key vault interaction program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+
+
 
 namespace dotnetcore
 {
@@ -6,7 +12,32 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string secretName = "MySecret";
+            string keyVaultName = "azrael";
+            var kvUri = "https://azrael.vault.azure.net";
+            SecretClientOptions options = new SecretClientOptions(){
+                Retry = {
+                    Delay = TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential                    
+                }
+            };
+            var client = new SecretClient(new Uri(kvUri),new DefaultAzureCredential(),options);
+            KeyVaultSecret secret = client.GetSecret(secretName);
+            Console.WriteLine("GETSECRET: "+ secret.Value);
+            Console.Write("Enter Secret >");
+            string secretValue = Console.ReadLine();
+            client.SetSecret(secretName, secretValue);
+            Console.Write("SetSecret");
+            Console.Write(" Key: ", secretName);
+            Console.Write(" Value: ", secretValue);
+            Console.WriteLine(" GETSECRRET ", secret.Value);
+
+            client.StartDeleteSecret(secretName);
+            Console.WriteLine("StartDeleteSecrete" + keyVaultName);
+
+            Console.WriteLine(" GETSECRRET ", secret.Value);
         }
     }
 }
